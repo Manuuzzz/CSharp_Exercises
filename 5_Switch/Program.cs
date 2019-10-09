31da0bbb5340f75f96b715eafd7275a1e0592f0a
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-09 09:51:53 +0200

    Switch

---
+++ 5_Switch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "benjamin";
+            const string niceName = "jean";
+            const string sillyName = "goofy";
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            switch (yourName.ToLower())
+            {
+                case myName: 
+                    Console.WriteLine("We have the same name");
+                    break;
+                
+                case niceName:
+                    Console.WriteLine("You have a nice name");
+                    break;
+
+                case sillyName:
+                    Console.WriteLine("You have a silly name");
+                    break;
+            }
+
+            Console.WriteLine($"Hello {yourName}!");
+            Console.ReadKey();
+        }
+    }
+}
