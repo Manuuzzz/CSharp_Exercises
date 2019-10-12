b6d3bbdade6a90d5fc4a19a3a5fb4e21d9d7ef1a
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-12 14:46:58 +0200

    Arrays exercise

---
+++ 9_Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames= { "Todd Anthony", "Kevin Holton", "Shane Laigle" };
+            int i;
+
+            Console.WriteLine($"Here are my {friendNames.Length} friends:");
+            for (i = 0; i < friendNames.Length; i++)
+            {
+                Console.WriteLine($"This is {friendNames[i]}");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
