a657947290d6b369114ca3a2d646e878c6af0846
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-12 14:41:19 +0200

    Struct exercise

---
+++ 7_Enumerations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Enumerations
+{
+    enum orientation : byte
+    {
+    north= 1,
+    east=2,
+    south= 3,
+    west= 4
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            orientation myOrientation = orientation.north;
+            Console.WriteLine($"My orientation is {myOrientation}");
+            Console.ReadKey();
+       
+        }
+    }
+}
