f2e68230fe8afffdb8556bbcca3bb726ff1d679f
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-12 16:35:14 +0200

    tochararray

---
+++ 11_Arraysc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Arraysc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello man";
+            char[] myChars = myString.ToCharArray();
+
+            foreach (char character in myChars)
+            {
+                Console.WriteLine($"{character} ");
+
+            }
+            Console.WriteLine($"{myChars[4]}");
+            Console.ReadKey();   
+        }
+        
+    }
+}
