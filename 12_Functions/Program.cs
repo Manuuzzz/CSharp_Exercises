8e5ceaecd6b09676246486f1bf72ecb2c410b4cf
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-15 20:38:26 +0200

    functions

---
+++ 12_Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Functions
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("This function writes to the console");
+        }
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+        }
+    }
+}
