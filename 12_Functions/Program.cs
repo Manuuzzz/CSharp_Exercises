0b935dc8ce282357d6c6b5b47482e698ca918f64
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-16 12:35:53 +0200

    Function - maxvalue array

--- 12_Functions/Program.cs
+++ 12_Functions/Program.cs
@@ -8,13 +8,30 @@ namespace _12_Functions
 {
     class Program
     {
+        static int MaxValue(int[] myArray)
+        {
+            int MaxValue = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] > MaxValue)
+                    MaxValue = myArray[i];
+
+            }
+            return MaxValue;
+
+        }
         static void Write()
         {
             Console.WriteLine("This function writes to the console");
         }
         static void Main(string[] args)
         {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int maxValue = 0;
+            maxValue = MaxValue(myArray);
+
             Write();
+            Console.WriteLine($"The maxvalue of the array is {maxValue}");
             Console.ReadKey();
         }
     }
