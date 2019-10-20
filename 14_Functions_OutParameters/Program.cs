1ce94eb10ba2368f4c7c2538f9728c4ca5ec3aac
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-20 10:42:45 +0200

    Functions - Out parameter

---
+++ 14_Functions_OutParameters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Functions_OutParameters
+{
+    class Program
+    {
+        static int MaxValue(int[] numberList, out int index)
+        {
+            int maxValue = 0;
+            index = 0;
+            for (int i = 0; i < numberList.Length; i++)
+            {
+               
+                if (numberList[i] > maxValue)
+                {
+                    maxValue = numberList[i];
+                    index = i;
+                }
+
+            }
+            return maxValue;
+       
+        }
+        static void Main(string[] args)
+        {
+            int[] numberlist = { 1, 5, 3, 8, 9, 4 };
+            int highestValue = MaxValue(numberlist, out int index);
+            Console.WriteLine($"The highest number of the array is {highestValue}, and is located at element {index+1}");
+            Console.ReadKey();
+        }
+    }
+}
