cc94ef446251717e7010f4e9dc64e7d884e522e0
Manuuzzz <emmanuel.lamon@gmail.com>
2019-10-21 20:05:35 +0200

    Function - commandline arguments

---
+++ 15_consoleapp_arguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_consoleapp_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //comand line arguments are given in the properties of the solution - debug window
+
+            Console.WriteLine($"This application took {args.Length} arguments");
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"{arg}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 15_consoleapp_arguments/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("15_consoleapp_arguments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("15_consoleapp_arguments")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f40e67e-49f9-4e2d-8be6-958d84b66b1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
