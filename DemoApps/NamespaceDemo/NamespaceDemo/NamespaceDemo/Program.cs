c9dd1e02075b402017685fa39fcbde56f97269e1
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-13 12:53:03 +0200

    upload namespace demo

---
+++ DemoApps/NamespaceDemo/NamespaceDemo/NamespaceDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace super
+{
+
+    class sampleclass {
+        public void samplemethod()
+        {
+
+            System.Console.WriteLine("samplemethod string");
+        }
+
+    }
+}
+
+
+namespace NamespaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            super.sampleclass outer = new super.sampleclass();
+            outer.samplemethod();
+            Console.ReadLine();
+
+        }
+    }
+
+   
+
+}
