104057dafdb292a90cae267e47ed1804e1bc0b7a
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-16 22:22:28 +0200

    create if else

--- csharp101/Branches_if/Branches_if/Branches_if/Program.cs
+++ csharp101/Branches_if/Branches_if/Branches_if/Program.cs
@@ -10,6 +10,23 @@ namespace Branches_if
     {
         static void Main(string[] args)
         {
+
+            int a = 1;
+            int b = 2;
+            int c = 4;
+            int ans = a + b + c;
+            if (a + b +c >= 10 && a == b){
+
+                Console.WriteLine($"The answer {ans} is >= 10 and a ({a}) == b ({b})");
+                Console.ReadLine();
+            }
+
+            else
+            {
+
+                Console.WriteLine($"The answer {ans} is not >= 10 and / or  a ({a}) != b ({b})");
+                Console.ReadLine();
+            }
         }
     }
 }
