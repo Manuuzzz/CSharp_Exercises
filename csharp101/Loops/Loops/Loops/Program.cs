c24c484cd054a7fb26c29cf2519fca5b175db6aa
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-17 11:07:10 +0200

    add challenge loops

--- csharp101/Loops/Loops/Loops/Program.cs
+++ csharp101/Loops/Loops/Loops/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,32 @@ namespace Loops
             {
                 Console.WriteLine("while 0 to 10");
                 x++;
+            }
+            Console.ReadLine();
+
+            //print all integers from 1 to 20 that are divisible by 3 - combine loops and branches
+
+            int sum = 0;
+
+            for (int z = 0; z <=20; z++)
+            {
+                
+                if (z % 3 == 0)
+                {
+
+                    Console.WriteLine($"{z} is divisible by 3");
+                    sum = sum + z;
+                 
+                }
+
+                else
+                {
+
+                    Console.WriteLine($"{z} is not divisible by 3");
+                    
+                }
+                Console.WriteLine($"sum is {sum}");
+               
             }
             Console.ReadLine();
         }
