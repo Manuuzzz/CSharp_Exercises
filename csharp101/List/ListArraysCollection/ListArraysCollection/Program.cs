7a2918c44169f5bbb8e8f0d69fb3b7962641a837
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-17 22:02:49 +0200

    List - remove - remove all

---
+++ csharp101/List/ListArraysCollection/ListArraysCollection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace ListArraysCollection
+{
+    class Program
+    {
+
+        private static bool testNumber(int i)
+        {
+
+            return (i == 7);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+           
+
+            var numbers = new List<int> { 1, 2, 3, 5, 7 , 7, 7};
+
+            foreach (var number in numbers) //you can replace var with int here
+            {
+                Console.WriteLine(number);
+                
+
+            }
+
+            numbers.Remove(3);
+            numbers.RemoveAll(testNumber);
+            foreach (int number in numbers) //you can replace var with int here
+            {
+               
+                Console.WriteLine(number);
+                
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
