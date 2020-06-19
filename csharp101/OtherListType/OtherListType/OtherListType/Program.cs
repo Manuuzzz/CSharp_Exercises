187714f968e9f594b2263d1c6bcfc4a9b76c9876
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-19 21:00:52 +0200

    List: Fibonacci sequence

---
+++ csharp101/OtherListType/OtherListType/OtherListType/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherListType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fibonaccilist = new List<int> { 1, 1 };
+
+            Console.WriteLine("Output the fibonacci sequence up to 676;");
+
+            while (fibonaccilist.Count < 20)
+            {
+
+                var previous = fibonaccilist[fibonaccilist.Count - 1];
+                var previous2 = fibonaccilist[fibonaccilist.Count - 2];
+
+                fibonaccilist.Add(previous + previous2);
+
+            }
+
+            foreach (var number in fibonaccilist)
+            {
+
+                Console.WriteLine(number);
+               
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
