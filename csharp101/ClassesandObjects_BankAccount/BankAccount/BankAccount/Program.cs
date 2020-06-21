94d77cb362d7785b28fd581abb8596f19f9d78ad
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-21 16:23:59 +0200

    create BankAccount - classes and objects

---
+++ csharp101/ClassesandObjects_BankAccount/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = new BankAccount("Jan", 1000);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.ReadLine();
+        }
+    }
+}
