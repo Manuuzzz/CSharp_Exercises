5d3fc514aff1ff50558077696927eb13457e069e
manu.lamon@hotmail.com <manu.lamon@hotmail.com>
2020-06-26 23:06:14 +0200

    Creating game snake
    Creating playing field with Windows Forms

---
+++ Snake/Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelGame = new System.Windows.Forms.Panel();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PanelGame
+            // 
+            this.PanelGame.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.PanelGame.Location = new System.Drawing.Point(64, 32);
+            this.PanelGame.Name = "PanelGame";
+            this.PanelGame.Size = new System.Drawing.Size(535, 378);
+            this.PanelGame.TabIndex = 0;
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(636, 57);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(118, 37);
+            this.ButtonStart.TabIndex = 1;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.PanelGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelGame;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
---
+++ Snake/Snake/Snake/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+
+        PictureBox[] snakeParts;
+        int snakeSize = 5;
+        Point location = new Point(120, 120);
+        string direction = "Right";
+        bool changingDirection = false;
+
+        PictureBox food = new PictureBox();
+        Point foodLocation = new Point(0, 0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            PanelGame.Controls.Clear();
+            snakeParts = null;
+            snakeSize = 5;
+            direction = "Right";
+            location = new Point(120,120);
+
+            drawSnake();
+           
+
+         
+        }
+
+        private void drawSnake()
+        {
+            snakeParts = new PictureBox[snakeSize];
+
+                for(int i = 0; i < snakeParts.Length; i++)
+                {
+                    snakeParts[i] = new PictureBox();
+                    snakeParts[i].Size = new Size(15, 15);
+                    snakeParts[i].BackColor = Color.Red;
+                    snakeParts[i].BorderStyle = BorderStyle.FixedSingle;
+                    snakeParts[i].Location = new Point(location.X - (15 * i), location.Y);
+                     PanelGame.Controls.Add(snakeParts[i]);
+                }
+
+
+            }
+
+                
+
+
+        
+    }
+}
